143674403c6160ccc0a94b41ff86641bfd80c2ae
LameloScript <140755836+LameloScript@users.noreply.github.com>
2024-03-17 01:24:45 +0000

    ajout de nouveau élément dans la derniere section du index.php - j'ai modifié les chemins d'acces aux images et fichier

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,168 +11,178 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <title>Databot - the next on future</title>
 </head>
+
 <body>
 
-<div class="bck">
-    <?php
-        include("../Databot-picki/Ressources/layouts/header.php");
-    ?>
-    <div class="sec-h">
-        <!-- <img class="arrow"  src="./Ressources/assets/arrow.svg" alt="arrow"></a>  -->
-        <span class="joy">Data-monitOr</span>
-    </div>
-    <div class="sec-d">
-        <div class="ga">
-            <h1>
-                surveillez vos espaces avec une technologie  de pointe
-            </h1>
-            <p>
-                Prenez dès aujourd'hui le controle de l'air que vous et votre
-                famille respirez grâce à DATABOT.
-                Commandez-le dès maintenant pour profiter
-                de cette technologie innovante.
-            </p>
-            <div class="btd">
-                <input type="button" class="cmd" value="Commandez dès maintenant">
-                <input type="button" class="en" value="En savoir plus">
+    <div class="bck">
+        <?php
+        include("./Ressources/layouts/header.php");
+        ?>
+        <div class="sec-h">
+            <!-- <img class="arrow"  src="./Ressources/assets/arrow.svg" alt="arrow"></a>  -->
+            <span class="joy">Data-monitOr</span>
+        </div>
+        <div class="sec-d">
+            <div class="ga">
+                <h1>
+                    surveillez vos espaces avec une technologie de pointe
+                </h1>
+                <p>
+                    Prenez dès aujourd'hui le controle de l'air que vous et votre
+                    famille respirez grâce à DATABOT.
+                    Commandez-le dès maintenant pour profiter
+                    de cette technologie innovante.
+                </p>
+                <div class="btd">
+                    <input type="button" class="cmd" value="Commandez dès maintenant">
+                    <input type="button" class="en" value="En savoir plus">
+                </div>
+            </div>
+            <div class="dr">
+                <img src="./Ressources/assets/databot-iphone.png" alt="databot-iphone">
             </div>
         </div>
-        <div class="dr">
-            <img src="../Databot-picki/Ressources/assets/databot-iphone.png" alt="databot-iphone">
-        </div>
+
     </div>
-    
-</div>
-<div class="sec-d1">
+    <div class="sec-d1">
         <div class="ga1">
             <p>data-monitor</p>
         </div>
-</div>
+    </div>
 
-<div class="sec-d2">
-    <div class="sec-dd2">
-        <div class="ga2">
-            <img src="../Databot-picki/Ressources/assets/int.png" alt="interogation-logo-3d">
-        </div>
-        <div class="ga3">
+    <div class="sec-d2">
+        <div class="sec-dd2">
+            <div class="ga2">
+                <img src="./Ressources/assets/int.png" alt="interogation-logo-3d">
+            </div>
+            <div class="ga3">
 
-            <h2>
-                pourquoi choisir nos services précis
-                de surveillance environnementale ?
-            </h2>
-            <div class="slider">
-                <div class="slide">
-                    <div class="box">
-                         <h4>
-                            collecte de données environnementales
-                         </h4>
-                         <p>
+                <h2>
+                    pourquoi choisir nos services précis
+                    de surveillance environnementale ?
+                </h2>
+                <div class="slider">
+                    <div class="slide">
+                        <div class="box">
+                            <h4>
+                                collecte de données environnementales
+                            </h4>
+                            <p>
                                 La puissante intelligence artificielle intégrée
-                                nos systèmes assurent l’analyse la plus avancée des données collectées. 
-                                Grâce à des algorithmes avancés, notre module est capable de détecter les 
+                                nos systèmes assurent l’analyse la plus avancée des données collectées.
+                                Grâce à des algorithmes avancés, notre module est capable de détecter les
                                 tendances, les pics d'émissions de CO2, les variations d'humidité
-                                les risques potentiels des UV et bien plus encore. Bénéficiez d’une 
-                                interprétation rapide et claire des données de données, mettant en 
+                                les risques potentiels des UV et bien plus encore. Bénéficiez d’une
+                                interprétation rapide et claire des données de données, mettant en
                                 évidence les points critiques pour une action immédiate.
-                         </p>
+                            </p>
+                        </div>
                     </div>
-                </div>
-                <div class="slide">
-                    <div class="box">
-                        <h4>
-                            analyse intelligente et interprétation des données
-                         </h4>
-                         <p>
-                                Grâce à notre puissante intelligence artificielle intégrée 
-                                artificielle intégrée, notre système assure une analyse 
-                                des données collectées. Nos algorithmes avancés 
-                                nous permettent de détecter avec précision les tendances, 
-                                les pics d'émission de CO2, les variations d'humidité, 
-                                les risques potentiels d'UV, et bien plus encore. 
-                                Vous bénéficiez d'une interprétation claire et rapide des données 
-                                Les données sont interprétées de manière claire et rapide, en mettant 
-                                en évidence les points critiques qui nécessitent une action immédiate. 
+                    <div class="slide">
+                        <div class="box">
+                            <h4>
+                                analyse intelligente et interprétation des données
+                            </h4>
+                            <p>
+                                Grâce à notre puissante intelligence artificielle intégrée
+                                artificielle intégrée, notre système assure une analyse
+                                des données collectées. Nos algorithmes avancés
+                                nous permettent de détecter avec précision les tendances,
+                                les pics d'émission de CO2, les variations d'humidité,
+                                les risques potentiels d'UV, et bien plus encore.
+                                Vous bénéficiez d'une interprétation claire et rapide des données
+                                Les données sont interprétées de manière claire et rapide, en mettant
+                                en évidence les points critiques qui nécessitent une action immédiate.
                                 qui nécessitent une action immédiate.
-                         </p>
+                            </p>
+                        </div>
                     </div>
-                </div>
-                <div class="slide">
-                    <div class="box">
-                         <h4>
+                    <div class="slide">
+                        <div class="box">
+                            <h4>
                                 visualisation intuitive et décisions proactives
-                         </h4>
-                         <p>
-                                Avec nos services, visualisez vos données en temps réel 
-                                sur une plateforme conviviale avec des dynamiques. 
-                                Restez connecté avec notre application mobile conviviale. Soyez proactif 
-                                grâce à l'accès à des données et des analyses approfondies 
-                                Identifier les problèmes, ajuster les conditions et assurer la sécurité environnementale. 
+                            </h4>
+                            <p>
+                                Avec nos services, visualisez vos données en temps réel
+                                sur une plateforme conviviale avec des dynamiques.
+                                Restez connecté avec notre application mobile conviviale. Soyez proactif
+                                grâce à l'accès à des données et des analyses approfondies
+                                Identifier les problèmes, ajuster les conditions et assurer la sécurité environnementale.
                                 et garantir la sécurité de l'environnement.
-                         </p>
+                            </p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="dots">
-                <span class="dot" data-slide="0"></span>
-                <span class="dot" data-slide="1"></span>
-                <span class="dot" data-slide="2"></span>
-            </div>
-        </div>
-    </div>
-        
-</div>
-<div class="sec-d3">
-    <div class="sec-dd2">  
-        <div class="ga4">
-            <div class="container">
-                <div class="boxi">
-                    <div class="box1" data-index="1">
-                        <div class="ttl">
-                            <h4>cO2</h4>
-                            <span>1</span>
-                        </div>
-                        <p>Le capteur de dioxyde de carbone (CO2) mesure les niveaux de ce gaz dans l'air. Le CO2 est essentiel à la photosynthèse, mais des niveaux élevés peuvent provoquer des problèmes de santé. C'est pourquoi il est important de surveiller les taux de CO2, surtout dans les espaces intérieurs fermés. Un capteur bien calibré permet cette surveillance et d'agir pour maintenir un air sain via une meilleure ventilation.</p>
-                    </div>
-                    <div class="box1" data-index="2">
-                        <div class="ttl">
-                            <h4>vOc</h4>
-                            <span>2</span>
-                        </div>
-                        <p>Le détecteur de composés organiques volatils (COV) évalue les substances présentes dans l'air qui sont essentiellement des impuretés ou des odeurs. Des niveaux élevés de COV peuvent influencer la composition de l'air que nous respirons, entraînant une irritation des yeux, des maux de tête, des problèmes de peau, etc.</p>
-                    </div>
+                <div class="dots">
+                    <span class="dot" data-slide="0"></span>
+                    <span class="dot" data-slide="1"></span>
+                    <span class="dot" data-slide="2"></span>
                 </div>
-                <div class="boxii">
-                    <div class="box1" data-index="3">
-                        <div class="ttl">
-                            <h4>Uv</h4>
-                            <span>3</span>
-                        </div>
-                        <p>Les rayons ultraviolets (UV) du soleil offrent des avantages indispensables, contribuant à la production de vitamine D et au maintien de nos rythmes biologiques. Cependant, une exposition excessive aux UV peut entraîner des problèmes tels que les coups de soleil, le vieillissement prématuré de la peau et un risque accru de cancer de la peau.</p>
-                    </div>
-                    <div class="box1" data-index="4">
-                        <div class="ttl">
-                            <h4>%</h4>
-                            <span>4</span>
-                        </div>
-                        <p>L'humidité atmosphérique est bonne pour les voies respiratoires si la peau, mais trop d'humidité favorise le développement de moisissures et d'allergies nocives pour la santé. C'est pourquoi il est essentiel de contrôler le niveau d'humidité via la ventilation et des déshumidificateurs. Cela permet d'éviter les problèmes de santé tout en profitant des bienfaits d'une vie agréable.</p>
-                    </div> 
-                </div>    
             </div>
         </div>
-        <div class="ga5">
-            <img src="../Databot-picki/Ressources/assets/pers.png" alt="interogation-logo-3d">
-        </div>
-    </div>
-        
-</div>
 
-<div class="sec-d2">
-    <h5>
-        prenez le contrôle absolu de la qualité de l'air que vous<br>
-        et vos proches. respirez chaque jour avec databot
-    </h4>
-</div>
+    </div>
+    <div class="sec-d3">
+        <div class="sec-dd2">
+            <div class="ga4">
+                <div class="container">
+                    <div class="boxi">
+                        <div class="box1" data-index="1">
+                            <div class="ttl">
+                                <h4>cO2</h4>
+                                <span>1</span>
+                            </div>
+                            <p>Le capteur de dioxyde de carbone (CO2) mesure les niveaux de ce gaz dans l'air. Le CO2 est essentiel à la photosynthèse, mais des niveaux élevés peuvent provoquer des problèmes de santé. C'est pourquoi il est important de surveiller les taux de CO2, surtout dans les espaces intérieurs fermés. Un capteur bien calibré permet cette surveillance et d'agir pour maintenir un air sain via une meilleure ventilation.</p>
+                        </div>
+                        <div class="box1" data-index="2">
+                            <div class="ttl">
+                                <h4>vOc</h4>
+                                <span>2</span>
+                            </div>
+                            <p>Le détecteur de composés organiques volatils (COV) évalue les substances présentes dans l'air qui sont essentiellement des impuretés ou des odeurs. Des niveaux élevés de COV peuvent influencer la composition de l'air que nous respirons, entraînant une irritation des yeux, des maux de tête, des problèmes de peau, etc.</p>
+                        </div>
+                    </div>
+                    <div class="boxii">
+                        <div class="box1" data-index="3">
+                            <div class="ttl">
+                                <h4>Uv</h4>
+                                <span>3</span>
+                            </div>
+                            <p>Les rayons ultraviolets (UV) du soleil offrent des avantages indispensables, contribuant à la production de vitamine D et au maintien de nos rythmes biologiques. Cependant, une exposition excessive aux UV peut entraîner des problèmes tels que les coups de soleil, le vieillissement prématuré de la peau et un risque accru de cancer de la peau.</p>
+                        </div>
+                        <div class="box1" data-index="4">
+                            <div class="ttl">
+                                <h4>%</h4>
+                                <span>4</span>
+                            </div>
+                            <p>L'humidité atmosphérique est bonne pour les voies respiratoires si la peau, mais trop d'humidité favorise le développement de moisissures et d'allergies nocives pour la santé. C'est pourquoi il est essentiel de contrôler le niveau d'humidité via la ventilation et des déshumidificateurs. Cela permet d'éviter les problèmes de santé tout en profitant des bienfaits d'une vie agréable.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="ga5">
+                <img src="./Ressources/assets/pers.png" alt="interogation-logo-3d">
+            </div>
+        </div>
+
+    </div>
+
+    <div class="sec-ddd2">
+        <h5>
+            prenez le contrôle absolu de la qualité de l'air que vous<br>
+            et vos proches. respirez chaque jour avec databot
+        </h5>
+        <p>
+            La qualité de l'air que vous respirez est trop importante pour être laissée au hasard. Avec Databoot,
+            <br>surveillez et agissez facilement pour garantir un air sain à votre famille.
+        </p>
+        <div class="contai">
+            <input type="email" placeholder="Entrez votre email" class="email-input">
+            <button class="start-button">Démarrer</button>
+        </div>
+    </div>
     <script src="./Ressources/js/header.js"></script>
     <script src="./Ressources/js/slider.js"></script>
 </body>
+
 </html>
